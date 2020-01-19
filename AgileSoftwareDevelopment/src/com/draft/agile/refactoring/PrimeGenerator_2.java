f3e24e42adba598b3eed7dcd17faa2bc5818f181
drafthj <ahuojiaa@qq.com>
2020-01-20 00:13:55 +0800

    《软件敏捷开发：原则、模式与设计》第5章重构 源码

--- AgileSoftwareDevelopment/src/com/draft/agile/refactoring/PrimeGenerator_2.java
+++ AgileSoftwareDevelopment/src/com/draft/agile/refactoring/PrimeGenerator_2.java
@@ -10,14 +10,55 @@ package com.draft.agile.refactoring;
  * @since [产品/模块版本] （可选）
  */
 public class PrimeGenerator_2 {
-    private static boolean[] f;
+    private static boolean[] isCrossed;
     private static int[] result;
 
     private static void initializeArrayOfIntegers(int maxValue) {
-        f = new boolean[maxValue + 1];
-        f[0] = f[1] = false;
-        for (int i = 2; i < f.length; i++) {
-            f[i] = true;
+        isCrossed = new boolean[maxValue + 1];
+        for (int i = 2; i < isCrossed.length; i++) {
+            isCrossed[i] = true;
         }
+    }
+
+    private static void crossOutMultiples() {
+        int maxPrimeFactor = calcMaxPrimeFactor();
+        for (int i = 2; i <= maxPrimeFactor; i++) {
+            if (notCrossed(i))
+                crossOutMultiplesOf(i);
+        }
+    }
+
+    private static int calcMaxPrimeFactor() {
+        Double maxPrimeFactor = Math.sqrt(isCrossed.length) + 1;
+        return maxPrimeFactor.intValue();
+    }
+
+    private static void crossOutMultiplesOf(int i) {
+        for (int multiple = 2 * i; multiple < isCrossed.length; multiple += i) {
+            isCrossed[multiple] = true;
+        }
+    }
+
+    private static boolean notCrossed(int i) {
+        return isCrossed[i] == false;
+    }
+
+    private static void putUnCrossedIntegersIntoResult() {
+        result = new int[numberOfUncrossedIntegers()];
+        for(int j = 0,i = 2; i < isCrossed.length; i++) {
+            if (notCrossed(i)) {
+                result[j++] = i;
+            }
+        }
+    }
+
+    private static int numberOfUncrossedIntegers() {
+        int count = 0;
+        for (int i = 2; i < isCrossed.length; i++) {
+            if (notCrossed(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
