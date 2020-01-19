d9d31e0f7ed32454f045e557bf7095678b282ac2
drafthj <17073828@cnsuning.com>
2020-01-19 19:15:46 +0800

    新功能: 《软件敏捷开发：原则、模式与设计》第5章重构 源码

---
+++ AgileSoftwareDevelopment/src/com/draft/agile/refactoring/Main.java
@@ -0,0 +1,18 @@
+package com.draft.agile.refactoring;
+
+import java.util.Arrays;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author drafthj
+ * @date 2020/1/19
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(GeneratePrimes.generatePrimes(100)));
+    }
+}
